682576ba71e9cff0f0416e2be5293f22d78c0bc5
juan david <jdacevedo.24@est.ucab.edu.ve>
2024-05-29 23:32:36 -0400

    feat: nuevos archivos y ejercicios

---
+++ Ejercicios/ejer_arreglos.c
@@ -0,0 +1,31 @@
+/*Ejercicio: Escriba un programa que lea 20 números y
+luego de asignarlos en un arreglo, determine en
+que posición se encuentra el máximo y el mínimo
+valor*/
+
+#include <stdio.h>
+
+int main(){
+    int num[20];
+    int max = num[0];
+    int min = num[0];
+    for(int i = 0; i < 20; i++){
+        printf("ingrese un numero [%d]: ", i+1);
+        scanf("%d", &num[i]);
+        if (max < num[i]){
+            max = num[i];
+        }else if (min > num[i])
+        {
+            min = num[i];
+        }
+        
+
+    }
+    printf("el max es: %d\n", max);
+    printf("el min es: %d\n", min);
+
+
+
+
+    return 0;
+}
---
+++ Practicas/arreglos.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+// EDADES
+
+/*
+
+int main(){
+    int i, edades[8];
+
+    for(i = 0; i < 8; i++){
+        printf("ingrese una edad: ");
+        scanf("%d", &edades[i]);
+    }
+
+    for(i = 0; i < 8; i++){
+        printf("edades[%i] = %i\n", i, edades[i]);
+    }
+
+    return 0;
+}
+*/
+
+// TEMPERATURA_HORA
+
+/*
+#include <stdio.h>
+#define ELEMENTOS 24
+int main() {
+int temp [ELEMENTOS];
+float media;
+int hora;
+for (hora=0; hora<ELEMENTOS ; hora++){
+printf( "Temperatura de las [%d]: ", hora);
+scanf( "%d", &temp [hora]);
+media +=temp [hora];
+}
+media = media /ELEMENTOS;
+printf("\nLa temperatura media es %f\n", media );
+}
+*/
+
